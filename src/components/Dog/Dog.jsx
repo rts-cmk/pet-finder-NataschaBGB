76b7244fdc190b8bcf51f412e67c281b0b8c79bd
NataschaBGB <nannabjerning@hotmail.com>
2025-11-17 14:09:51 +0100

    fix: changed some styling in the Dog component to match fig style

--- src/components/Dog/Dog.jsx
+++ src/components/Dog/Dog.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router";
 
 import dog from '../../assets/dog.jpg'
+import { GrLocation } from "react-icons/gr";
 import { IoMdHeartEmpty } from "react-icons/io";
 
 import './Dog.sass'
@@ -8,21 +9,25 @@ import './Dog.sass'
 export default function Dog() {
 
     return (
-        <Link to={"/details"}>
-            <section className='pet-finder__dog'>
+        <Link to={"/details"} className='pet-finder__dog'>
 
                 <img src={dog} alt="" className='pet-finder__dog-image' />
             
-                <article className='pet-finder__dog-description'>
+                <article className='pet-finder__dog-content'>
+                    
                     <div className='pet-finder__dog-title'>
                         <h1>Breed</h1>
-                        <p>location</p>
+                        <div className="dog-location">
+                            <GrLocation className="location-icon" />
+                            <p>location</p>
+                        </div>
                     </div>
-                    <p>short description</p>
+
+                    <p className="pet-finder__dog-description">short description</p>
+
                     <IoMdHeartEmpty className='pet-finder__dog-heart' />
                 </article>
                     
-            </section>
         </Link>
     )
 
